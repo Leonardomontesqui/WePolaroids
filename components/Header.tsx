680f88fb4f17404d8ad055c116e4da3467f685a0
Leonardo <lmontesq@uwaterloo.ca>
2025-01-12 15:19:18 -0500

    semi-mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="flex flex-col p-5 absolute z-50 mt-4 mx-4 gap-2 items-center rounded-xl w-[274px]  bg-cream">
-      <h1 className="text-[96px] text-[#7A7A7A] text-blue font-bold leading-none w-full">
+    <div className="flex flex-col md:p-5 p-3 absolute z-20 mt-4 mx-4 gap-2 items-center rounded-xl w-[200px] md:w-[274px]  bg-cream">
+      <h1 className="md:text-[96px] text-[32px] text-[#7A7A7A] text-blue font-bold leading-none w-full">
         We.
       </h1>
-      <p className="justify-left text-[#63635D] text-[20px] font-semibold w-full">
+      <p className="justify-left text-[#63635D] md:text-[20px] text-[14px] font-semibold w-full">
         everyones photo album
       </p>
     </div>
